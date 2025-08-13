a0b0717c92084f95376704c5b103af15e1def40d
naimatullahRezaee <naimatullahrezaee@gmail.com>
2025-08-13 13:50:53 +0430

    add routes to the app page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Routes, Route } from "react-router";
 import "./App.css";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/Home/HomePage";
 
 function App() {
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="checkout" element={<h1>Hello World</h1>} />
+      </Routes>
     </>
   );
 }
