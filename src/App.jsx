e518bbee4a4fa5a48cb5df4d8e4025d655f97703
naimatullahRezaee <naimatullahrezaee@gmail.com>
2025-08-13 14:25:05 +0430

    the design of this project done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router";
 import "./App.css";
 import HomePage from "./pages/Home/HomePage";
 import CheckOutPage from "./pages/CheckOut/CheckOutPage";
+import OrdersPage from "./pages/Orders/OrdersPage";
+import TrackingPage from "./pages/Tracking/TrackingPage";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route index element={<HomePage />} />
         <Route path="checkout" element={<CheckOutPage />} />
+        <Route path="orders" element={<OrdersPage />} />
+        <Route path="tracking" element={<TrackingPage />} />
       </Routes>
     </>
   );
--- src/pages/CheckOut/CheckOutPage.jsx
+++ src/pages/CheckOut/CheckOutPage.jsx
@@ -1,3 +1,4 @@
+import Header from "../../components/Header";
 import "./CheckOutPage.css";
 import "./CheckoutHeader.css";
 
@@ -5,28 +6,7 @@ const CheckOutPage = () => {
   return (
     <>
       <title> CheckOut </title>
-      <div className="checkout-header">
-        <div className="header-content">
-          <div className="checkout-header-left-section">
-            <a href="index.html">
-              <img className="logo" src="images/logo.png" />
-              <img className="mobile-logo" src="images/mobile-logo.png" />
-            </a>
-          </div>
-
-          <div className="checkout-header-middle-section">
-            Checkout (
-            <a className="return-to-home-link" href="index.html">
-              3 items
-            </a>
-            )
-          </div>
-
-          <div className="checkout-header-right-section">
-            <img src="images/icons/checkout-lock-icon.png" />
-          </div>
-        </div>
-      </div>
+      <Header />
 
       <div className="checkout-page">
         <div className="page-title">Review your order</div>
